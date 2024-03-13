f802ad31f907f4005704cd41376c03278dda457d
michalspoko18 <151867988+michalspoko18@users.noreply.github.com>
2024-03-13 22:48:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import time
 
 def write_current_time_to_file(filename):
-    while True:
         current_time = time.strftime("%H:%M:%S")  # Pobranie aktualnej godziny w formacie HH:MM:SS
         print(current_time)
         with open(filename, 'a') as file:  # Otwarcie pliku w trybie dopisywania
             file.write(current_time + '\n')  # Dopisanie aktualnej godziny do pliku
-        time.sleep(10)  # Poczekanie 10 sekund
 
 if __name__ == "__main__":
     filename = "current_time.txt"
